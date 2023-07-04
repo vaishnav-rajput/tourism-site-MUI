ac0e306d13f0925b5ab2a8c344d555ad70e5b595
vaishnav <vaishnav.rajput108@gmail.com>
2023-07-04 21:30:11 +0530

    TourCard created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import TourCard from "./components/TourCard";
+import Container from '@mui/material/Container';
+// import {Container, Grid} from "@mui/material";
+import Grid from '@mui/material/Grid';
+
+function App() {
+  return (
+ <div>
+  <Container>
+    <TourCard/>
+  </Container>
+ </div>
+  );
+}
+
+export default App;
---
+++ src/components/TourCard.js
@@ -0,0 +1,13 @@
+import Paper from '@mui/material/Paper';
+
+
+const TourCard = () => {
+    return(
+        <div>
+            <Paper elevation={3} variant='outlined' square>Hello</Paper>
+        
+        </div>
+    )
+}
+
+export default TourCard;
